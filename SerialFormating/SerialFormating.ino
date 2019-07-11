76245522ab462347fc7650a449a2c9707dc635b2
Zack Palmer <zacke.palmer@gmail.com>
2019-07-10 21:11:10 -0500

    Create SerialFormating.ino

---
+++ SerialFormating/SerialFormating.ino
@@ -0,0 +1,20 @@
+/* SerialFormating.ino
+ * Board: Sparkfun RedBoard
+ * Author: Neko'z
+ * Date: July 10, 2019
+ */
+
+// Setup is a built in function that gets called once
+// initially upon start-up or reset on the arduino.
+void setup() {
+    // Start the Serial interface (uses digital pins 0 
+    // and 1) at a baud rate (bits per second) of 9600
+    Serial.begin(9600);
+}
+
+// Loop is a built in function that gets called repeatedly
+// after the setup has completed. The main program should
+// written here.
+void loop() {
+    
+}
